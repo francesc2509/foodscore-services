633c749b560f85359ac4e100901220c36f199b97
Francesc Manarel Bueno <24278635+shinwook95@users.noreply.github.com>
2019-02-04 22:17:24 +0100

    Removed dist folder

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./App");
-App_1.default.listen(App_1.default.get('port'), () => console.log(`Server running on http://${App_1.default.get('host')}:${App_1.default.get('port')}`));
-//# sourceMappingURL=server.js.map
